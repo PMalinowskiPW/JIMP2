fcfe6f2a64a1225c0e0847c68124f6a58f51a131
elzbietakar <ela_pw@LAPTOP-9FLMPFDV>
2024-04-14 13:04:58 +0200

    zmiana zapisu, dodanie 0

--- format_transformer/format_transformer.c
+++ format_transformer/format_transformer.c
@@ -37,101 +37,115 @@ int is_crossing (int row, int col, char ** arr) {
 }
 
 void transform_maze_format(FILE *in, FILE *out, Maze *maze) {
+     int line_counter;
+
     int counter = 1;
     int row, col;
     row = col = 1;
 
     char ** arr = load_data(in, maze);
     fprint_Maze (out, maze);
-
+ 
     //jeśli początek znajduje się "w ścianie", to trzeba nadpisać go z użyciem innych zasad! todo
 
     while (row < maze->rows) {
+        line_counter = 2;
         if (is_crossing(row, col, arr)) {
             fprintf(out, "%i %i ", row, col);
-
+        
             //up
             while ( row - counter >= 0 ) {
                 //wyjątek -> zamiast ściany jest 'K', czyli wyjście z labiryntu, przyjmujemy wagę "w ścianę" = 1
                 if (arr[row - counter][col] == 'K') {
                     fprintf(out, "%i %i %i", row - counter, col, 1);
+                    line_counter += 3;
                     break;
                 }
-
+                
                 if (counter == 1) {
-                    if (arr[row - counter][col] == 'X')
+                    if (arr[row - counter][col] == 'X') 
                         break;
                 } else if (is_crossing(row - counter +1, col, arr)) {
                         fprintf(out, "%i %i %i ", row - counter +1, col, (counter -1)/2);
+                        line_counter += 3;
                         break;
                 }
-                //jeśli nie ma ściany ani nie jest skrzyżowaniem, to szukamy wyżej
-                counter += 2;
+                //jeśli nie ma ściany ani nie jest skrzyżowaniem, to szukamy wyżej 
+                counter += 2; 
             }
-            //reset
+            //reset 
             counter = 1;
 
             //right
             while ( col + counter <= maze->cols ) {
                     if (arr[row][col + counter] == 'K') {
                         fprintf(out, "%i %i %i ", row, col + counter, 1);
+                        line_counter += 3;
                         break;
                     }
-
+                    
                     if (counter == 1) {
-                        if (arr[row][col + counter] == 'X')
+                        if (arr[row][col + counter] == 'X') 
                             break;
                     } else if (is_crossing(row, col + counter -1, arr)) {
                             fprintf(out, "%i %i %i ", row, col + counter -1, (counter-1)/2);
+                            line_counter += 3;
                             break;
                     }
-                counter += 2;
+                counter += 2; 
             }
             counter = 1;
-
+    
             //down
             while ( row + counter <= maze->rows ) {
                 if (arr[row + counter][col] == 'K') {
                     fprintf(out, "%i %i %i ", row + counter, col, 1);
+                    line_counter += 3;
                     break;
                 }
 
                 if (counter == 1) {
-                    if (arr[row + counter][col] == 'X')
+                    if (arr[row + counter][col] == 'X') 
                         break;
                 } else if (is_crossing(row + counter -1, col, arr)) {
                         fprintf(out, "%i %i %i ", row + counter -1, col, (counter -1)/2);
+                        line_counter += 3;
                         break;
                 }
-                counter += 2;
+                counter += 2; 
             }
             counter = 1;
-
-
+    
+    
             //left
             while ( col - counter >= 0) {
                 if (arr[row][col - counter] == 'K') {
                     fprintf(out, "%i %i %i ", row, col - counter, 1);
+                    line_counter += 3;
                     break;
                 }
                 if (counter == 1) {
-                    if (arr[row][col - counter] == 'X')
+                    if (arr[row][col - counter] == 'X') 
                         break;
                 } else if (is_crossing(row, col - counter +1, arr)) {
-                        fprintf(out, "%i %i %i ", row, col - counter +1, (counter-1)/2);
+                        fprintf(out, "%i %i %i ", row, col - counter +1, (counter -1)/2);
+                        line_counter += 3;
                         break;
                 }
-                counter += 2;
+                counter += 2; 
             }
             counter = 1;
+            
+            for (int i = line_counter; i < 14; i++)
+                fprintf(out, "0 ");
 
             fprintf(out, "\n");
         }
-
+        
         if (col == maze->cols -2){
             col = 1;
-            row += 2;
-        } else
+            row += 2; 
+        } else 
             col += 2;
 
     }
